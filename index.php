20fe3d00413290c6482c80a37ac815014d9ab198
pp19dd <pp19dd@gmail.com>
2015-04-09 15:02:47 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html xml:lang="fa" lang="fa">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="fa" />
+<link rel="stylesheet" href="reset.css" type="text/css" />
+<title>Aggregator</title>
+<script>
+</script>
+</head>
+<body>
+<div id="content" dir="rtl">
+test
+</div>
+</body>
+</html>
