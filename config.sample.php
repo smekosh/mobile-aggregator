d80b1f81318a6912e9626fb715a8f141d4964535
pp19dd <pp19dd@gmail.com>
2015-04-09 16:34:47 -0400

    Added config variable for local

--- config.sample.php
+++ config.sample.php
@@ -4,5 +4,6 @@ $config = array(
     "rss" => array(
         "http://example1.com/rss/",
         "http://example2.com/rss/"
-    )
+    ),
+    "local" => "http://localhost/mobile-aggregator/local/"
 );
