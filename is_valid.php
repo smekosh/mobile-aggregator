57fb37a0417f8136ff770efed7d33458ba750366
pp19dd <pp19dd@gmail.com>
2015-05-29 14:15:29 -0400

    Removed URL restriction

--- is_valid.php
+++ is_valid.php
@@ -2,13 +2,13 @@
 
 /*
 CLI program
-php -f is_valid.php url
+php -f is_valid.php http://url/
 
 returns "yes" or "no"
 */
 
-if( !isset($argc) ) die( "this is a CLI program :\\");
-if( $argc <> 2 ) die( "need a url" );
+if( !isset($argc) ) die( "this is a CLI program :\\\n");
+if( $argc <> 2 ) die( "need a url :\\\n" );
 
 require( "vendor/autoload.php" );
 require( "config.php" );
@@ -17,7 +17,7 @@ require( "modifier_hostname.php" );
 
 date_default_timezone_set('Europe/Prague');
 
-$url = $config["local"] . $argv[1];
+$url = $argv[1];
 
 try {
     $feed = Feed::loadRss($url);
